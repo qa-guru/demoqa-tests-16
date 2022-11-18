59019794526eaabb42fd2c641580c01822a50123
svasenkov <aanher@gmail.com>
2022-11-18 20:43:03 +0300

    added homework

--- src/test/java/examples/CssXpathExamples.java
+++ src/test/java/examples/CssXpathExamples.java
@@ -1,3 +1,5 @@
+package examples;
+
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
---
+++ src/test/java/tests/RegistrationWithCommentsTests.java
@@ -0,0 +1,64 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationWithCommentsTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+//        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void successfulRegistrationTest() {
+        String userName = "Alex";
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue(userName);
+        $("#lastName").setValue("Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+//        $("#gender-radio-1").click(); // wrong
+//        $("#gender-radio-3").parent().click(); // good
+//        $(byText("Other")).click(); // not very good
+        $("#genterWrapper").$(byText("Other")).click(); // best
+//        $("label[for=gender-radio-3]").click(); // good
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+//        $(".react-datepicker__month-select").selectOptionByValue("6");
+        $(".react-datepicker__year-select").selectOption("2008");
+// <div class="react-datepicker__day--030 react-datepicker__day--outside-month">30</div>
+// <div class="react-datepicker__day--030"                                     >30</div>
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+//        $x("//*[@class='react-datepicker__day--030'][not(contains(@class, 'react-datepicker__day--outside-month'))]").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+//        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+        $("#currentAddress").setValue("Some address 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+//        $("#react-select-3-option-0").click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(userName), text("Egorov"),
+                text("alex@egorov.com"), text("1234567890"));
+    }
+}
