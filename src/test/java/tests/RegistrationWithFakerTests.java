234a7566436c9fbc3d5ef6d93f9fe72e9a9d876e
svasenkov <aanher@gmail.com>
2022-11-22 21:45:16 +0300

    added Faker

---
+++ src/test/java/tests/RegistrationWithFakerTests.java
@@ -0,0 +1,69 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static tests.TestData.*;
+import static utils.RandomUtils.*;
+
+public class RegistrationWithFakerTests extends TestBase {
+
+    @Test
+    void successfulRegistrationTest() {
+        Faker faker = new Faker(new Locale("fr"));
+//        Faker faker = new Faker();
+        String userName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String userEmail = faker.internet().emailAddress();
+        String userNumber = faker.phoneNumber().subscriberNumber(10);
+        String currentAddress = faker.lebowski().quote();
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue(userName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("#genterWrapper").$(byText("Other")).click(); // best
+        $("#userNumber").setValue(userNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(userName), text(lastName),
+                text(userEmail), text(userNumber), text(currentAddress));
+    }
+
+    @Test
+    void someRegistrationTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue(userName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+       // todo
+    }
+}
